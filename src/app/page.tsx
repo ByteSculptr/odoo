656a31b4c317db30fcbc7c3cfd47d99b043bfee4
Himanshu Arora <ha0himanshuarora@gmail.com>
2025-08-02 04:39:59 +0000

    The sidebar dowsn't change color and also the login/signup pages don't w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,11 +66,11 @@ export default function LoginPage() {
 
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background dark:bg-card">
+    <div className="flex items-center justify-center min-h-screen bg-background">
        <div className="absolute top-4 right-4">
             <ThemeToggle />
        </div>
-       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] dark:bg-background"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)] dark:bg-[radial-gradient(circle_500px_at_50%_200px,rgba(41,133,255,0.1),transparent)]"></div></div>
+       <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] dark:bg-background"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)] dark:bg-[radial-gradient(circle_500px_at_50%_200px,rgba(41,133,255,0.1),transparent)]"></div></div>
       <Card className="w-full max-w-sm shadow-2xl">
         <CardHeader>
           <div className="flex justify-center mb-4">
